2573b970f4d4ceae4f725b6014aae4c13646c751
billy <lijun526757124@163.com>
2020-03-10 19:07:07 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,44 @@
+<style lang="less">
+  @import './index.less';
+</style>
+<template>
+  <div class="login">
+    <div class="login-con">
+      <Card icon="log-in" title="欢迎登录" :bordered="false">
+        <div class="form-con">
+          <Form ref="loginForm">
+            <FormItem prop="userName">
+              <Input placeholder="请输入用户名">
+                <span slot="prepend">
+                  <Icon :size="16" type="ios-person"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem prop="password">
+              <Input type="password" placeholder="请输入密码">
+                <span slot="prepend">
+                  <Icon :size="14" type="md-lock"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem>
+              <Button  type="primary" long>登录</Button>
+            </FormItem>
+          </Form>
+          <p class="login-tip"></p>
+        </div>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+</style>
